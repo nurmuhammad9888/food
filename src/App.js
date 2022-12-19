b32cf76c0de282dd4eab291c30decb01a63fd44b
Nurmuhammad <juaqobilovnurmuhammad9888@gmail.com>
2022-12-19 17:37:45 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './assets/styles/main.css';
+import { Logo, Home } from "./assets/images/icons/icons"
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello world</h1>
+      <Logo/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
